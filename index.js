fee361d626da5feb5976bd72d60ecad002cb5927
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-05 17:59:20 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b) {
+  // we'll fill this in shortly
+  return a+b;
+}
+function subtract(a,b) {
+    // we'll fill this in shortly
+    return a-b;
+  }
+  function divide(a,b){
+      return a/b;
+
+  }
+
+  function multiply(a,b){
+return a*b;
+  }
+  function increment(n){
+    
+    return  ++n;
+  }
+  function decrement(n){
+    
+    return  --n;
+  }
+
+  function makeInt(n){
+   return parseInt(n,10);
+  }
+  function  preserveDecimal(n){
+  return   parseFloat(n)
+  }
